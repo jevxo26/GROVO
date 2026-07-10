b87fdc514662b34eafd7682dcc25a78c09d20968
AbdurRahman11072 <rjrahman014@gmail.com>
2026-07-11 05:07:23 +0600

    chore: add instrauction on home page

--- src/app/(homeLayout)/page.tsx
+++ src/app/(homeLayout)/page.tsx
@@ -2,7 +2,20 @@ import { ModeToggle } from "@/components/ui/modeToggle";
 
 export default function Home() {
   return (
-    <div>
+    <div className="justify-center items-center p-20">
+      <h1>
+        Hi Developer Make sure you to add all the components with the help of
+        claud ai.
+      </h1>
+      <h1>Also make sure to add dark theme color to every components. </h1>
+      <h1>
+        So that if the theme is changed it looks good. Also only work on
+        dashboard not in the homepage.
+      </h1>
+      <h1>
+        Also Create any folder on (dashboardLayout)/dashboard folder. Not on
+        dashboardLayout(This is very important).
+      </h1>
       <ModeToggle />
     </div>
   );
