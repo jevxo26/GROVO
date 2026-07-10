4949f92127609da68d97848977854252581f616a
AbdurRahman11072 <rjrahman014@gmail.com>
2026-07-11 04:55:28 +0600

    fix: generated file creation directory feat: added theme toggle button on home page

--- src/app/(homeLayout)/page.tsx
+++ src/app/(homeLayout)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/modeToggle";
 
 export default function Home() {
   return (
     <div>
-      <Button>This is home button</Button>
+      <ModeToggle />
     </div>
   );
 }
