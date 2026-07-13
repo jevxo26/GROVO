4baa6e088c42e0663cba30eb73f568438d42729a
AbdurRahman11072 <rjrahman014@gmail.com>
2026-07-13 13:01:43 +0600

    feat: refactor Notification module to controller-service pattern

--- server/routes/notification.routes.ts
+++ server/routes/notification.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { NotificationController } from "../controllers/notification.controller";
+import { notificationController } from "../controllers/notification.controller";
 
 const router = Router();
 
-router.post("/queue", NotificationController.queueAlert);
-router.get("/user/:userId", NotificationController.getActiveAlerts);
+router.post("/queue", notificationController.queueAlert);
+router.get("/user/:userId", notificationController.getActiveAlerts);
 
 export const notificationRouter = router;
+
