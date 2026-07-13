a907722fb331716602a0c98ddd5b2e01814578f0
Md Antor Mia <mdantormia1779@gmail.com>
2026-07-14 03:12:30 +0600

    branch info page and branch info card and budget card add

--- src/app/(dashboardLayout)/dashboard/staf/branchinfo/page.tsx
+++ src/app/(dashboardLayout)/dashboard/staf/branchinfo/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import BranchInfoCard from '../Components/BranchInfoCard/BranchInfoCard';
+import BudgetUtilization from '../Components/BudgetUtilization/BudgetUtilization';
 
 const Branchinfo = () => {
     return (
-        <div className='max-w-7xl mx-auto p-6'>
-            <BranchInfoCard></BranchInfoCard>
+        <div className='max-w-7xl mx-auto p-6 space-y-6'>
+            {/* Branch Details */}
+            <BranchInfoCard />
+            
+            {/* Budget Analytics */}
+            <BudgetUtilization />
         </div>
     );
 };
