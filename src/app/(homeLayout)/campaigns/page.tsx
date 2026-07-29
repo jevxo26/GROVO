8087bf4a5ba3ec10b095a4965136cae82c3ee045
Md Antor Mia <mdantormia1779@gmail.com>
2026-07-30 02:16:41 +0600

    campaings page done

--- src/app/(homeLayout)/campaigns/page.tsx
+++ src/app/(homeLayout)/campaigns/page.tsx
@@ -1,9 +1,11 @@
 import { ActiveCampaignsHeader } from "@/components/ui/ActiveCampaignsHeader";
 import React from "react";
+import CampaignsSection from './../../../components/ui/CampaignsSection';
 
 const Campaigns = () => {
   return <div>
     <ActiveCampaignsHeader />
+    <CampaignsSection />
   </div>;
 };
 
