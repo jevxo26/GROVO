3e31a13f0a6d417a4d20e5dd3163cc1d7883fb8f
Pijush <psagorsarker512@gmail.com>
2026-07-29 14:51:33 +0600

    Add donars page in Distric Coordinator panel

---
+++ src/app/(dashboardLayout)/dashboard/@districtcoordinator/donors/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CoordinatorDonors from '../../Components/coordinator/coordinatorDonors'
+
+const page = () => {
+  return (
+    <CoordinatorDonors/>
+  )
+}
+
+export default page
