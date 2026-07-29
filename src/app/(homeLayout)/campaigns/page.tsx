41fc3bc4c1a00eeade84a526a9583c2a184ee73d
Md Antor Mia <mdantormia1779@gmail.com>
2026-07-30 02:01:20 +0600

    campain banner added

--- src/app/(homeLayout)/campaigns/page.tsx
+++ src/app/(homeLayout)/campaigns/page.tsx
@@ -1,7 +1,10 @@
+import { ActiveCampaignsHeader } from "@/components/ui/ActiveCampaignsHeader";
 import React from "react";
 
 const Campaigns = () => {
-  return <div>campaigns page</div>;
+  return <div>
+    <ActiveCampaignsHeader />
+  </div>;
 };
 
 export default Campaigns;
