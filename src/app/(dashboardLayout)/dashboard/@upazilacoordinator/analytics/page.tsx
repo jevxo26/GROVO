9821cbe88bcd2dcc2779a5f3af369e93d09e004b
Pijush <psagorsarker512@gmail.com>
2026-07-29 16:51:39 +0600

    Update and fix Canpaigns page in Coordinator dashboard

---
+++ src/app/(dashboardLayout)/dashboard/@upazilacoordinator/analytics/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CoordinatorAnalytics from '../../Components/coordinator/coordinatorAnalytics'
+
+const page = () => {
+  return (
+    <CoordinatorAnalytics/>
+  )
+}
+
+export default page
---
+++ src/app/(dashboardLayout)/dashboard/@upazilacoordinator/fieldActivities/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CoordinatorFieldActivities from '../../Components/coordinator/coordinatorFieldActivities'
+
+const page = () => {
+  return (
+    <CoordinatorFieldActivities/>
+  )
+}
+
+export default page
