f60be32862753b8ac4b07ef9cace9660fba47d5e
Md Antor Mia <mdantormia1779@gmail.com>
2026-07-30 00:02:01 +0600

    banner added sucessfully

--- src/app/(homeLayout)/page.tsx
+++ src/app/(homeLayout)/page.tsx
@@ -1,23 +1,12 @@
-import Footer from "@/components/ui/footer/footer";
-import { HeadingText } from "@/components/common/headingText/headingText";
-import { ModeToggle } from "@/components/ui/modeToggle";
-import ReduxTest from "@/redux/ReduxTest";
+
+import ImpactSection from '@/components/ui/ImpactSection';
+import Banner from './../../components/ui/Banner';
 
 export default function Home() {
   return (
-    <div className="justify-center items-center p-20">
-      <section className="py-12">
-        <HeadingText
-          badge="📊 OUR IMPACT IN NUMBERS"
-          title="Making a Difference,"
-          highlight="One Life at a Time"
-          description="Every number represents a story of hope, a life touched, and a community transformed."
-        />
-      </section>
-
-      <ModeToggle />
-      <ReduxTest></ReduxTest>
+    <div>
+      <Banner />
+      <ImpactSection />
     </div>
-
   );
 }
