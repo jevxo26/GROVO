bbdf2b7eaec01aa5057b42b313baf8ddea26f0f8
AbdurRahman11072 <rjrahman014@gmail.com>
2026-08-06 09:32:17 +0600

    feat: implement root layout with global providers and dedicated home layout structure

--- src/app/(homeLayout)/layout.tsx
+++ src/app/(homeLayout)/layout.tsx
@@ -1,11 +1,18 @@
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+
 const HomeLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <>
-  {children}
-  </>
+  return (
+    <>
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default HomeLayout;
